51ccbe32c5c0aa36f7968b8d342d5d70a60a5016
Yash Hegde <yash4success@gmail.com>
2024-12-23 17:44:09 +0530

    send button ui

--- src/components/Chat/GroupChat/GroupChat.tsx
+++ src/components/Chat/GroupChat/GroupChat.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { ChevronRightCircle } from "lucide-react";
 import { Dispatch, SetStateAction } from "react";
 
 interface GroupChatProps {
@@ -18,13 +20,30 @@ const GroupChat: React.FC<GroupChatProps> = ({
       <div className="absolute top-4 right-4 bg-white p-2 rounded shadow">
         <div>Latest message: {latestMessage}</div>
       </div>
-      <div className="absolute top-16 right-4 bg-white p-2 rounded shadow">
+      <div className="absolute top-16 right-4 bg-white p-2 rounded shadow flex justify-center items-center space-x-2 pr-2">
         <input
           value={userMsg}
           onChange={(e) => setUserMsg(e.target.value)}
           placeholder="Type a message"
+          className="border rounded-md placeholder:p-2 p-1"
         />
-        <button onClick={sendMessage}>Send</button>
+
+        {!userMsg ? (
+          <Button
+            onClick={sendMessage}
+            className="bg-inherit shadow-none hover:bg-inherit"
+            disabled={true}
+          >
+            <ChevronRightCircle className="text-black" />
+          </Button>
+        ) : (
+          <Button
+            onClick={sendMessage}
+            className="bg-inherit shadow-none hover:bg-inherit"
+          >
+            <ChevronRightCircle className="text-black" />
+          </Button>
+        )}
       </div>
     </div>
   );
